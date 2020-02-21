d5c15df2797c5fa648df87038d3f4ef34beffdd8
David Martínez Echavarría <dmartinez@arsys.es>
2020-02-21 09:11:58 +0100

    Added new aliases.

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -4,8 +4,10 @@
 # @brief Contiene alias utiles para el resto de scripts y el uso normal de bash
 
 alias bashrc='source ~/.bashrc'
+
 alias ll='ls -l'
-alias la='ls -la'
+alias la='ls -lah'
+alias lt='ls --human-readable --size -1 -S --classify'
 
 alias cd..='cd ..'
 alias ..='cd ..'
